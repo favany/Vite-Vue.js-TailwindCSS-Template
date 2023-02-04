0d47e0442b9f477f10317fc7e06e81f58bd44ad6
Romance Ucely <faaa@live.com>
2023-02-04 12:32:10 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div className="contents relative h-screen justify-center">
+    <div className="text-blue-600 dark:text-sky-400 font-big text-3xl text-center">
+      Hello, this is Vue.js + Tailwind CSS, build by Vite!
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
